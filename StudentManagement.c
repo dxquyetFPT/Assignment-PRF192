06b8d877475798056c918a4c76cd4e20fec53c72
dxquyet16 <quyetcoder16@gmail.com>
2024-03-04 13:52:58 +0700

    change core of the code

--- StudentManagement.c
+++ StudentManagement.c
@@ -1,287 +1,271 @@
-/*
-	project : student management
-	author: Group 2
-	class: SE1940
-	version: 1.0
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
+#define N_MAX 100
 
-const int N_MAX = 100;
+char id[N_MAX][20];
+char name[N_MAX][255];
+double diemTb[N_MAX];
+double diemToan[N_MAX];
+double diemLy[N_MAX];
+double diemHoa[N_MAX];
 
-struct student {
-	char id[20];
-	char name[255];
-	double diemTb;
-	double diemToan;
-	double diemLy;
-	double diemHoa;
-};
-
-typedef struct student student_t;
+int size = 0;
 
 int exist(char *);
-student_t convertDataFromString(char *);
-int processFileRead(char *,int *,student_t *);
-int processWriteFile(char *,int,student_t *);
+void convertDataFromString(char *, int);
+int processFileRead(char *);
+int processWriteFile(char *);
 void displayMenu();
-void addNewStudent(student_t* ,int *);
-void printListOfStudent(student_t *,int );
-void searchStudentById(student_t*,int);
-void calculateTheAverageScore(student_t*,int );
-void sortListOfStudentByAverageScore(student_t*,int );
+void addNewStudent();
+void printListOfStudent();
+void searchStudentById();
+void calculateTheAverageScore();
+void sortListOfStudentByAverageScore();
 
 /*
 	author: Duong Xuan Quyet
- 	function : exist
+	function : exist
 	description: check file exist
 */
 int exist(char *fileName)
 {
-	int existed = 0;
 	FILE *f = fopen(fileName, "r");
 	if (f != NULL)
 	{
-		existed = 1;
 		fclose(f);
+		return 1;
 	}
-	return existed;
+	return 0;
 }
-
 
 /*
 	author: Duong Xuan Quyet
- 	function : convertDataFromString
-	description: convert data from string to object student
+	function : convertDataFromString
+	description: convert data from string to array
 */
-student_t convertDataFromString(char *s){
-	student_t objStudent;
-	int i=0;
-	int j=0;
-	while(s[i]!=';'){
-		objStudent.id[j]=s[i];
+void convertDataFromString(char *s, int index)
+{
+	int i = 0;
+	int j = 0;
+	while (s[i] != ';')
+	{
+		id[index][j] = s[i];
 		i++;
 		j++;
 	}
-	objStudent.id[j]='\0';
+	id[index][j] = '\0';
 	i++;
-	j=0;
-	while(s[i]!=';'){
-		objStudent.name[j]=s[i];
+	j = 0;
+	while (s[i] != ';')
+	{
+		name[index][j] = s[i];
 		i++;
 		j++;
 	}
-	objStudent.name[j]='\0';
+	name[index][j] = '\0';
 	char mark[255];
-	j=0;
+	j = 0;
 	i++;
-	while(s[i]!=';'){
+	while (s[i] != ';')
+	{
 		mark[j] = s[i];
 		i++;
 		j++;
 	}
-	mark[j]='\0';
+	mark[j] = '\0';
 	char *endptr;
-	objStudent.diemToan = strtod(mark,&endptr);
+	diemToan[index] = strtod(mark, &endptr);
 	char ly[255];
-	j=0;
+	j = 0;
 	i++;
-	while(s[i]!=';'){
+	while (s[i] != ';')
+	{
 		mark[j] = s[i];
 		i++;
 		j++;
 	}
-	mark[j]='\0';
-	objStudent.diemLy = strtod(mark,&endptr);
-	j=0;
+	mark[j] = '\0';
+	diemLy[index] = strtod(mark, &endptr);
+	j = 0;
 	i++;
-	while(s[i]!=';'){
+	while (s[i] != ';')
+	{
 		mark[j] = s[i];
 		i++;
 		j++;
 	}
-	mark[j]='\0';
-	objStudent.diemHoa = strtod(mark,&endptr);
-	objStudent.diemTb = (objStudent.diemToan+objStudent.diemLy+objStudent.diemHoa)/3.0f;
-	return objStudent;
+	mark[j] = '\0';
+	diemHoa[index] = strtod(mark, &endptr);
+	diemTb[index] = (diemToan[index] + diemLy[index] + diemHoa[index]) / 3.0f;
 }
 
 /*
 	author: Duong Xuan Quyet
- 	function : processFileRead
+	function : processFileRead
 	description: read file students.in and get data to the array
 */
-int processFileRead(char *filename,int *size,student_t* listOfStudent)
+int processFileRead(char *filename)
 {
-    char c; 
-	if (exist(filename) == 0)
+	if (!exist(filename))
 	{
 		printf("The file %s does not exist.\n", filename);
 		return 0;
 	}
 	FILE *f = fopen(filename, "r");
 	char line[255];
-	student_t objStudentInput;
-	while (fgets(line, sizeof(line), f)) {
-		objStudentInput = convertDataFromString(line);
-		if(*size<N_MAX){
-			listOfStudent[*size]=objStudentInput;
-			(*size)++;
-		}
+	while (fgets(line, sizeof(line), f))
+	{
+		convertDataFromString(line, size);
+		size++;
 	}
 	fclose(f);
-	printf("Read file students.in successful!\n");
+	printf("Read file %s successful!\n", filename);
 	return 1;
 }
 
 /*
 	author: Duong Xuan Quyet
- 	function : processWriteFile
+	function : processWriteFile
 	description: write data all student to file students.out
 */
-int processWriteFile(char *filename,int size,student_t* listOfStudent)
+int processWriteFile(char *filename)
 {
 	FILE *f = fopen(filename, "w");
-	if (f == NULL) {
-	    printf("can't open file'.\n");
-	    return 0;
+	if (f == NULL)
+	{
+		printf("Can't open file.\n");
+		return 0;
 	}
-	fflush(stdin); 
-	int i=0,j=0;
+	int i = 0;
 	fprintf(f, "===== ID =========== Ho ten ========= Toan == Ly == Hoa == Diem trung binh ====\n");
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < strlen(listOfStudent[i].name); j++) {
-            if (listOfStudent[i].name[j] < 0 || listOfStudent[i].name[j] > 127) {
-                listOfStudent[i].name[j] = ' ';
-            }
-        }
-        fprintf(f, "%-12s | %-20s | %2.2lf | %2.2lf | %2.2lf | %2.2lf\n", 
-                listOfStudent[i].id, 
-                listOfStudent[i].name,
-                listOfStudent[i].diemToan, 
-                listOfStudent[i].diemLy, 
-                listOfStudent[i].diemHoa, 
-                listOfStudent[i].diemTb);
-    }
-    printf("file saved!\n");
-    fclose(f);
+	for (i = 0; i < size; i++)
+	{
+		fprintf(f, "%-12s | %-20s | %2.2lf | %2.2lf | %2.2lf | %2.2lf\n",
+				id[i], name[i], diemToan[i], diemLy[i], diemHoa[i], diemTb[i]);
+	}
+	printf("File saved!\n");
+	fclose(f);
 	return 1;
 }
 
-
 /*
 	author: Duong Xuan Quyet
- 	function : displayMenu
+	function : displayMenu
 	description: print menu to screen
 */
-void displayMenu() {
-	printf("=============================================================");
-    printf("\nMenu:\n");
-    printf("1. Them sinh vien moi\n");
-    printf("2. In danh sach sinh vien\n");
-    printf("3. Tim kiem sinh vien theo ID\n");
-    printf("4. Tinh diem trung binh cua tat ca sinh vien\n");
-    printf("5. Sap xep danh sach sinh vien theo diem trung binh\n");
-    printf("0. Thoat\n");
+void displayMenu()
+{
+	printf("=============================================================\n");
+	printf("Menu:\n");
+	printf("1. Them sinh vien moi\n");
+	printf("2. In danh sach sinh vien\n");
+	printf("3. Tim kiem sinh vien theo ID\n");
+	printf("4. Tinh diem trung binh cua tat ca sinh vien\n");
+	printf("5. Sap xep danh sach sinh vien theo diem trung binh\n");
+	printf("0. Thoat\n");
 }
-
 
 /*
 	author: Tran Minh Quang
- 	function : addNewStudent
+	function : addNewStudent
 	description: add new student to array
 */
-void addNewStudent(student_t* listOfStudent,int *size){
-	// write code here
+void addNewStudent()
+{
+	// Write code here
 }
-
 
 /*
 	author: Le Hai Nam
- 	function : printListOfStudent
+	function : printListOfStudent
 	description: print list of student to screen
 */
-void printListOfStudent(student_t* listOfStudent,int size){
-	// write code here
+void printListOfStudent()
+{
+	// Write code here
 }
-
 
 /*
 	author: Nong Hoang Duy Anh
- 	function : searchStudentsById
+	function : searchStudentsById
 	description: search student by id and print description about student
 */
-void searchStudentById(student_t* listOfStudent,int size){
-	// write code here
+void searchStudentById()
+{
+	// Write code here
 }
-
 
 /*
 	author: Duong Trong Luc
- 	function : calculateTheAverageScore
+	function : calculateTheAverageScore
 	description: print The Average Score of all student
 */
-void calculateTheAverageScore(student_t* listOfStudent,int size){
-	// write code here
+void calculateTheAverageScore()
+{
+	// Write code here
 }
 
 /*
 	author: Tran Thanh Hai
- 	function : sortListOfStudentByAverageScore
+	function : sortListOfStudentByAverageScore
 	description: sort list of Student by Average Score don't change root array and print list to screen
 */
-void sortListOfStudentByAverageScore(student_t* listOfStudent,int size){
-	// write code here
+void sortListOfStudentByAverageScore()
+{
+	// Write code here
 }
 
-int main(){
-	student_t listOfStudent[N_MAX];
+int main()
+{
 	char fi[] = "students.in";
 	char fo[] = "students.out";
-	int size = 0;
-	if (!processFileRead(fi, &size, listOfStudent)) {
-		printf("read file fail\n");
-        return 1;
-    }
-    
-    int choice;
-    do {
-        displayMenu();
-        printf("Nhap lua chon cua ban: ");
-        scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                addNewStudent(listOfStudent,&size);
-                break;
-            case 2:
-                printListOfStudent(listOfStudent,size);
-                break;
-            case 3:
-                searchStudentById(listOfStudent,size);
-                break;
-            case 4:
-                calculateTheAverageScore(listOfStudent,size);
-                break;
-            case 5:
-                sortListOfStudentByAverageScore(listOfStudent,size);
-                break;
-            case 0:
-                printf("Tam biet!\n");
-                break;
-            default:
-                printf("Lua chon khong hop le. Vui long nhap lai.\n");
-                break;
-        }
-    } while (choice != 0);
-    
-    if (!processWriteFile(fo,size, listOfStudent)) {
-        printf("write file fail\n");
-        return 1;
-    }
+	if (!processFileRead(fi))
+	{
+		printf("Read file fail\n");
+		return 1;
+	}
+
+	int choice;
+	do
+	{
+		displayMenu();
+		printf("Nhap lua chon cua ban: ");
+		scanf("%d", &choice);
+
+		switch (choice)
+		{
+		case 1:
+			addNewStudent();
+			break;
+		case 2:
+			printListOfStudent();
+			break;
+		case 3:
+			searchStudentById();
+			break;
+		case 4:
+			calculateTheAverageScore();
+			break;
+		case 5:
+			sortListOfStudentByAverageScore();
+			break;
+		case 0:
+			printf("Tam biet!\n");
+			break;
+		default:
+			printf("Lua chon khong hop le. Vui long nhap lai.\n");
+			break;
+		}
+	} while (choice != 0);
+
+	if (!processWriteFile(fo))
+	{
+		printf("Write file fail\n");
+		return 1;
+	}
 	return 0;
 }
